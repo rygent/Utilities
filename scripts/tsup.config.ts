eb5873e439a84da016656eeb2a381ae8cfa59b13
Rygent <marizky65@gmail.com>
2022-09-21 23:37:46 +0700

    chore(*): initial commit

---
+++ scripts/tsup.config.ts
@@ -0,0 +1,33 @@
+import { relative, resolve } from 'node:path';
+import { defineConfig, type Options } from 'tsup';
+
+export const createTsupConfig = ({
+	globalName = undefined,
+	format = ['esm', 'cjs', 'iife'],
+	target = 'es2021',
+	sourcemap = true,
+	dts = true,
+	esbuildOptions = (options, context) => {
+		if (context.format === 'cjs') {
+			options.banner = {
+				js: '"use strict";'
+			};
+		}
+	}
+}: ConfigOptions = {}) =>
+	defineConfig({
+		clean: true,
+		dts,
+		entry: ['src/index.ts'],
+		format,
+		minify: false,
+		skipNodeModulesBundle: true,
+		sourcemap,
+		target,
+		tsconfig: relative(__dirname, resolve(process.cwd(), 'src', 'tsconfig.json')),
+		keepNames: true,
+		globalName,
+		esbuildOptions
+	});
+
+type ConfigOptions = Pick<Options, 'esbuildOptions' | 'sourcemap' | 'target' | 'format' | 'globalName' | 'dts'>;
