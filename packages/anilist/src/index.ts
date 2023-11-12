3a9491e25648f0a17b42b0a63eeabd3ab9ddcf5a
Rygent <46462403+Rygent@users.noreply.github.com>
2023-11-12 08:42:33 +0700

    feat: add `getAnime` and `getManga` method

--- packages/anilist/src/index.ts
+++ packages/anilist/src/index.ts
@@ -1,5 +1,5 @@
 import type { AnilistResponse, SearchType } from './lib/types/Anilist.js';
-import { endpoint } from './lib/constants.js';
+import { AnimeFragment, MangaFragment, endpoint } from './lib/constants.js';
 import { resolveQueryFragment } from './lib/util.js';
 import axios, { AxiosError } from 'axios';
 
@@ -14,6 +14,32 @@ export class Anilist {
 
 		try {
 			const body = JSON.stringify({ query: resolveQueryFragment(type), variables: { search, page, perPage } });
+			const response = await axios.post(endpoint, body, { headers: { 'Content-Type': 'application/json' } });
+
+			return response.data as AnilistResponse;
+		} catch (error: unknown) {
+			throw new Error(`Received status ${(error as AxiosError).status} (${(error as AxiosError).message})`);
+		}
+	}
+
+	public async getAnime(variables: { id: number }): Promise<AnilistResponse> {
+		const { id } = variables;
+
+		try {
+			const body = JSON.stringify({ query: AnimeFragment, variables: { id } });
+			const response = await axios.post(endpoint, body, { headers: { 'Content-Type': 'application/json' } });
+
+			return response.data as AnilistResponse;
+		} catch (error: unknown) {
+			throw new Error(`Received status ${(error as AxiosError).status} (${(error as AxiosError).message})`);
+		}
+	}
+
+	public async getManga(variables: { id: number }): Promise<AnilistResponse> {
+		const { id } = variables;
+
+		try {
+			const body = JSON.stringify({ query: MangaFragment, variables: { id } });
 			const response = await axios.post(endpoint, body, { headers: { 'Content-Type': 'application/json' } });
 
 			return response.data as AnilistResponse;
