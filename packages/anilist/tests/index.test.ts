46093bb2c876ca09d98f1fa85ca650aa5a8080eb
Rygent <46462403+Rygent@users.noreply.github.com>
2023-11-13 08:52:19 +0700

    chore: cleanup

--- packages/anilist/tests/index.test.ts
+++ packages/anilist/tests/index.test.ts
@@ -1,20 +1,20 @@
 import { Anilist } from '../dist/index';
 import { isClass } from '@sapphire/utilities';
 
+const anilist = new Anilist();
+
 describe('Anilist', () => {
 	test('Anilist should be a class', () => {
 		expect(isClass(Anilist)).toBe(true);
 	});
 
 	test('Search anime "Cowboy Bebop" should be defined', async () => {
-		const anilist = new Anilist();
 		const response = await anilist.search({ type: 'anime', search: 'Cowboy Bebop' });
 
 		expect(response.data.Page.media![0]).toBeDefined();
 	});
 
 	test('Search manga "Naruto" should be defined', async () => {
-		const anilist = new Anilist();
 		const response = await anilist.search({ type: 'manga', search: 'Naruto' });
 
 		expect(response.data.Page.media![0]).toBeDefined();
